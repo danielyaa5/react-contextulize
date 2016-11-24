766e434b4b96b7d0a0e821a7684e00c851a9744c
Daniel Yak <danielyaa5@gmail.com>
2016-11-24 15:56:12 -0800

    create menuItem id by concatenating menuId and itemId

--- src/ContextMenu.js
+++ src/ContextMenu.js
@@ -143,7 +143,7 @@ export default class ContextMenu extends Component {
 
                 return (
                     <button
-                        id= { `react-contextmenu-item-${menuItem.itemId }` }
+                        id= { `${this.props.menuId}-${menuItem.itemId }` }
                         className="react-contextmenu-item"
                         key={ menuItem.itemId }
                         { ...menuItemProps }>
